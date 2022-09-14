a8aac759c2776b324e892a2a0fc9878e506bb03b
ichengi <39175857+ichengi@users.noreply.github.com>
2022-09-14 11:14:16 +0800

    fix:取消时差设置

--- EarlyWarning.py
+++ EarlyWarning.py
@@ -45,7 +45,7 @@ def judge_api(api_url:list)->str:
         time_get_str = api_data["data"].get("gettime") # 获取时间
         time_get = datetime.datetime.strptime(time_get_str,"%Y-%m-%d %H:%M:%S") # 转换格式
         now_time = datetime.datetime.now()
-        if (now_time+datetime.timedelta(hours=7)) <= time_get <= (now_time+datetime.timedelta(hours=9)): # 判断是在一小时内获取的
+        if (now_time-datetime.timedelta(hours=1)) <= time_get <= (now_time+datetime.timedelta(hours=1)): # 判断是在一小时内获取的
             pass
         else:
             errmsg = f"接口:{api_url}获取时间超时,最近一次获取是在{time_get},当前时间为{now_time},比较时间为{now_time+datetime.timedelta(hours=7)},{now_time+datetime.timedelta(hours=9)}"
