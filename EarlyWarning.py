04bc3024e971c7774631bbc6343fa9368af77af9
ichengi <39175857+ichengi@users.noreply.github.com>
2022-09-21 11:39:28 +0800

    Update EarlyWarning.py

--- EarlyWarning.py
+++ EarlyWarning.py
@@ -66,11 +66,12 @@ def judge_api(api_url:list)->str:
     # region 数据中台,定时发送短信
     elif "authorization" in api_url:
         api_data = get_json(api_url)  # 获取数据
-        status = api_data['data']['data'][0]['status']
-        if status == "Executed":
-            pass
-        else:
-            errmsg = f"今日份定时发送短信失败，请尽快处理"
+        errmsg = api_data
+#         status = api_data['data']['data'][0]['status']
+#         if status == "Executed":
+#             pass
+#         else:
+#             errmsg = f"今日份定时发送短信失败，请尽快处理"
     #endregion
     # region 水文数据
     elif "hydraulic" in api_url:
